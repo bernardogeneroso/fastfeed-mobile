178ffd8d4d892f174e82d02a361e160fe25cbc20
Bernardo Generoso <58465456+bernardogeneroso@users.noreply.github.com>
2021-03-18 23:05:48 +0000

    Add on multer a new storage about package signature

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -10,6 +10,10 @@ interface IUploadConfig {
   multerStorageUsersAvatars: {
     storage: StorageEngine;
   };
+
+  multerStoragePackageSignature: {
+    storage: StorageEngine;
+  };
 }
 
 export default {
@@ -21,6 +25,18 @@ export default {
       filename: (request, file, callback) => {
         const fileHash = crypto.randomBytes(10).toString("hex");
         const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  multerStoragePackageSignature: {
+    storage: multer.diskStorage({
+      destination: path.resolve(uploadsFolder, "package_signature"),
+      filename: (request, file, callback) => {
+        const fileHash = crypto.randomBytes(10).toString("hex");
+        const fileName = fileHash;
 
         return callback(null, fileName);
       },
